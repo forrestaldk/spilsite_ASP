5f5401dfefc86020550acaa9889069f84ea99edf
Torben Storm Jensen <tstormdk@gmail.com>
2014-07-01 13:44:47 +0200

    select * i metode

---
+++ db_selects.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="db_selects.aspx.cs" Inherits="db_selects" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Repeater ID="repeater_brugere" runat="server">
+        <ItemTemplate>
+            <article>
+                BrugerNavn: <%#Eval("bruger_username") %><br />
+                BrugerRolle:<%#Eval("bruger_navn") %><hr />
+            </article>
+        </ItemTemplate>
+    </asp:Repeater>
+
+</asp:Content>
+
---
+++ db_selects.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class db_selects : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        HentBrugere();
+    }
+    private SqlConnection Conn()
+    { 
+        SqlConnection conn = new SqlConnection();
+        //["ConnectionString"] fås fra min webconfig-fil copy/paste navnet
+        conn.ConnectionString = ConfigurationManager.ConnectionStrings["DBCONN"].ToString();
+        conn.Open();
+
+        return conn;
+    } 
+    private void HentBrugere()
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = Conn();
+        cmd.CommandText = "SELECT * FROM bruger";
+
+        DataTable dt = new DataTable();
+        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+        adapter.Fill(dt);
+        repeater_brugere.DataSource = dt;
+        repeater_brugere.DataBind();
+
+    }
+}
