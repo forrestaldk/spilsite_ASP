33e9dbd42c8befe83d1659726bfe76106adacd9a
Torben Storm Jensen <tstormdk@gmail.com>
2014-07-02 12:59:50 +0200

    form indsat
    Ændret på css styles til at passe .net

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -9,6 +9,27 @@ public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        bool LoggedIn = login();
+        Session["bruger_navn"] = "Hans";
 
+        if (LoggedIn == true)
+        {
+            LoggedInView.ActiveViewIndex = -1;
+            login_besked.Text = "Velkommen " + Session["bruger_navn"].ToString();
+        }
+        else
+        {
+            LoggedInView.ActiveViewIndex = 0;
+            login_besked.Text += "Velkommen Gæst";
+        }
     }
+
+    private bool login(string user, string pass)
+    {
+        bool loggedIn = false;
+
+        return loggedIn;
+    }
+
+
 }
